c8761b16b2b1819fb98c86f882a3804e4254b0c2
Timur Kristóf <venemo@msn.com>
2018-12-19 10:11:09 +0100

    fix decoder tests

--- smog_ref/decode/dec_usage.c
+++ smog_ref/decode/dec_usage.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "dec_ref.h"
+#include "ao40_decode_message.h"
 
 void test_decode_data() {
   FILE *fp;
-  uint8_t raw[RAW_SIZE];
-  uint8_t data[DATA_SIZE];
+  uint8_t raw[AO40_RAW_SIZE];
+  uint8_t data[AO40_DATA_SIZE];
   int8_t error[2];
 
   fp = fopen("fec", "rb");
   fread(raw, sizeof(uint8_t), sizeof(raw), fp);
   fclose(fp);
  
-  decode_data(raw, data, error);
+  ao40_decode_data(raw, data, error);
 
   printf("Errors: RS[0] = %d, RS[1] = %d\n", error[0], error[1]);
   fp = fopen("dec_data", "wb");
@@ -22,18 +22,18 @@ void test_decode_data() {
 
 void test_decode_data_debug() {
   FILE *fp;
-  uint8_t raw[RAW_SIZE];          // Raw data to be decoded
-  uint8_t conv[CONV_SIZE];        // Deinterleaved raw block with SYNC removed
-  uint8_t dec_data[RS_SIZE];      // Viterbi output: RS blocks interleaved and scrambled
-  uint8_t rs[2][RS_BLOCK_SIZE];   // RS blocks without leading padding zeros
-  uint8_t data[DATA_SIZE];        // Decoded data
+  uint8_t raw[AO40_RAW_SIZE];          // Raw data to be decoded
+  uint8_t conv[AO40_CONV_SIZE];        // Deinterleaved raw block with SYNC removed
+  uint8_t dec_data[AO40_RS_SIZE];      // Viterbi output: RS blocks interleaved and scrambled
+  uint8_t rs[2][AO40_RS_BLOCK_SIZE];   // RS blocks without leading padding zeros
+  uint8_t data[AO40_DATA_SIZE];        // Decoded data
   int8_t  error[2];               // RS block decoded and corrected errors (or -1 if unrecoverable error occured)
 
   fp = fopen("fec", "rb");
   fread(raw, sizeof(uint8_t), sizeof(raw), fp);
   fclose(fp);
  
-  decode_data_debug(raw, data, error, conv, dec_data, rs);
+  ao40_decode_data_debug(raw, data, error, conv, dec_data, rs);
 
   printf("Errors: RS[0] = %d, RS[1] = %d\n", error[0], error[1]);
   
